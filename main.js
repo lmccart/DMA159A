d6ac525298c15c29e9984b8cb0e3e14b2369a8d5
Lauren McCarthy <laurmccarthy@gmail.com>
2018-03-13 18:14:48 -0700

    ok

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
 
+  var hash = window.location.hash;
+  if (hash && $(hash).length) {
+    openSection(hash);
+  }
+
   $('#links .button').click(function() {
-    var selected = $(this).data('id');
-    $('.block').hide();
-    $('#'+selected).show();
+    var selected = '#'+$(this).data('id');
+    openSection(selected);
   })
 });
+
+function openSection(id, heading) {
+  $('.block').hide();
+  $(id).show();
+  window.location.hash = id.substring(1);
+  if (!heading) {
+    $(window).scrollTop(0);
+  } else {
+    $(window).scrollTop($("#"+heading).offset().top-30);
+  }
+}
